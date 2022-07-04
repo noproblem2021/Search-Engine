090fe48040cb41456a9407c42003e00651f86384
Toshiba Vishnoi <82307223+noproblem2021@users.noreply.github.com>
2022-07-04 13:18:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,190 @@
+body
+{
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto',sans-serif;
+    background-color: black;
+}
+*
+{
+    box-sizing: inherit;
+    transition-property: all;
+    transition-duration: .6s;
+    transition-timing-function: ease;
+}
+header
+{
+    width: 100%;
+}
+ul
+{
+    list-style-type: none;
+}
+.nav
+{
+    float: right;
+}
+.nav li
+{
+    display: inline-block;
+    padding: 6px;
+}
+.nav_links
+{
+    color: white;
+}
+.nav_links:hover
+{
+    text-decoration: underline;
+}
+.sign_in
+{
+    background: rgb(85, 81, 81);
+    margin-right: 20px;
+    padding: 7px 15px;
+    border-radius: 3px;
+    font-weight: bold;
+}
+.sign_in:hover
+{
+    box-shadow: 1px 1px 5px white;
+}
+.sign_in
+{
+    color: #fff;
+}
+a
+{
+    text-decoration: none;
+    color: inherit;
+}
+.logo
+{
+    text-align: center;
+    display: block;
+    margin: 0 auto;
+    clear: both;
+    padding-top: 112px;
+    padding-bottom: 20px;
+}
+.form
+{
+    text-align: center;
+}
+.form_search
+{
+    width: 400px;
+    line-height: 32px;
+    padding: 5px 4px;
+}
+.form .form_search
+{
+    padding: 0 8px;
+}
+.container
+{
+    text-align: center;
+}
+.button
+{
+    margin-top: 25px;
+    margin-bottom: 290px;
+    display: inline-block;
+    cursor: pointer;
+}
+.glow-on-hover {
+    font-size: 16px;
+    width: 130px;
+    height: 26px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg,black,white);
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: 100%;
+    height: 100%;
+    animation: glowing 8s linear infinite;
+    opacity: 0;
+    transition: opacity .5s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+@keyframes glowing {
+    from{
+        background-position: 0 0;
+    }
+    to
+    {
+        background-position: 400% 0;
+    }
+}
+footer
+{
+    background: #f2f2f2;
+    border-top: solid 2px #e4e4e4;
+    bottom: 0;
+    padding-bottom: 0;
+    width: 100%
+}
+footer li
+{
+    color: white;
+    display:inline-block;
+    font-size: 14px;
+    padding: 3px;
+}
+footer ul
+{
+    float: left;
+    padding: 2px;
+    word-spacing: 1.5em;
+}
+footer ul a:hover
+{
+    text-decoration: underline;
+}
+.footer_left
+{
+    float: left;
+}
+.footer_right
+{
+    float: right;
+}
